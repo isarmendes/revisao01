98076367349930262af357490d57633db9aa7538
isarmendes <isadoramendes@aluno.senai.br>
2023-08-17 10:32:08 -0300

    feat: create CategoryService and addCategory  method

--- revisao-01/assests/js/script.category-product.js
+++ revisao-01/assests/js/script.category-product.js
@@ -1,8 +1,32 @@
+// categoria possui vários produtos =>1 :n
 class Category{
     constructor(id,name){
         this.id = id;
         this.name =name;
+        this.products=[];
     }
 }
 
-// categoria possui vários produtos =>1 :n
+// Produto pertence a uma categoria =>1:1
+class Products{
+    constructor(id,name,price,category){
+        this.id= id;
+        this.name = name;
+        this.price = price;
+        this.category =  category;
+    }
+}
+
+class CategoryService {
+    constructor(){
+        this.categories =[];
+        this.nextCategoryId = 0;
+    }
+
+    addCategory(name){
+        const id = this.nextCategoryId ++; 
+        const category = new Category(id, name);
+        this.categorie.push(category);
+    }
+}
+
