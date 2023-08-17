0cc04b303cf319c97e79e2afdc8cfaaee3e59b79
isarmendes <isadoramendes@aluno.senai.br>
2023-08-17 10:49:37 -0300

    feat: create createCategory function

--- revisao-01/assests/js/script.category-product.js
+++ revisao-01/assests/js/script.category-product.js
@@ -26,7 +26,17 @@ class CategoryService {
     addCategory(name){
         const id = this.nextCategoryId ++; 
         const category = new Category(id, name);
-        this.categorie.push(category);
+        this.categories.push(category);
     }
 }
 
+const categoryList = new CategoryService();
+
+function createCategory(){
+    const categoryName = "Doce";
+
+    categoryList.addCategory(categoryName);
+
+    console.log(categoryList.categories);
+
+}
